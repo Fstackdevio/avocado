8206c9ef5d7b1c0ac18dd632425179deb7bb1491
david adegoke <davidadegoke31@gmail.com>
2017-08-17 21:31:20 +0100

    Refactored folder structure

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+/*const gulp = require('gulp');
+
+
+var paths = {
+	scripts: ['./app/js/**/*.js', './app/js/*.js'],
+	styles: ['./app/styles/**/*.scss', './app/styles/*.scss','./app/styles/**/*.css', './app/styles/*.css'],
+}*/
